4f2c3286194536ee16068eb2cbf1bfa9fb426648
otaiga <karl.turner5@gmail.com>
2012-12-07 11:29:50 +0000

    updated for changes made in api - authkey

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,9 +2,9 @@ class NotificationsController < ApplicationController
 
   def retieve_notification
     posted =  JSON.parse(request.body.read.as_json)
-    @msisdn = posted["msisdn"]
+    @auth_key = posted["auth_key"]
     @country = posted["country"]
-    @user = User.where(:msisdn => @msisdn).first
+    @user = User.find_by_auth_key(@auth_key)
     @user.country = @country
     @user.save
 
