7bfb1bcb3eb20b0f289c98717794c1f8d0e5e77a
otaiga <karl.turner5@gmail.com>
2013-03-21 16:08:18 +0000

    updated code to reflect changes in push notifications, and upgraded rails versions, etc

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -3,7 +3,10 @@ class NotificationsController < ApplicationController
   def retieve_notification
     posted =  JSON.parse(request.body.read.as_json)
     @auth_key = posted["auth_key"]
-    @country = posted["country"]
+    @country = posted["dialing_code"]
+    @iso_code = posted["iso_3366_1"]
+    #terrible - will remove later
+    puts "received code : #{@iso_code}"
     @user = User.find_by_auth_key(@auth_key)
     @user.country = @country
     @user.save
--- spec/controllers/notifications_controller_spec.rb
+++ spec/controllers/notifications_controller_spec.rb
@@ -5,7 +5,7 @@ describe NotificationsController do
   it "should be able to parse the correct notification and save country" do
     create_user
     given_user.update_attributes(auth_key: given_auth_key)
-    body = {auth_key: given_auth_key, country: "33", timestamp: "2012-01-01T12:37:46+01:00"}
+    body = {auth_key: given_auth_key, dialing_code: "33", iso_3366_1: "FR", timestamp: "2012-01-01T12:37:46+01:00"}
     @request.env['RAW_POST_DATA'] = body.to_json
     post :retieve_notification
     given_user.reload
